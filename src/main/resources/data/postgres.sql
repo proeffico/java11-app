9a6e80cb92bf6cecae1febe21624b375b5c5a092
Thinh Dinh <thinh.dinh@bisnode.com>
2019-11-01 09:17:30 +0200

    Preparing for deployment

--- src/main/resources/data/postgres.sql
+++ src/main/resources/data/postgres.sql
@@ -1,4 +1,5 @@
 -- create table answer
+
 CREATE TABLE answer(
    answer_id serial PRIMARY KEY,
    answer text NOT NULL
@@ -21,7 +22,9 @@ CREATE TABLE question(
    question_id serial PRIMARY KEY,
    question text NOT NULL,
    fk_questionary_id serial,
-   CONSTRAINT question_fk_questionary_id FOREIGN KEY (fk_questionary_id) REFERENCES questionary (questionary_id)
+   fk_question_type_id serial,
+   CONSTRAINT question_fk_questionary_id FOREIGN KEY (fk_questionary_id) REFERENCES questionary (questionary_id),
+   CONSTRAINT question_fk_question_type_id FOREIGN KEY (fk_question_type_id) REFERENCES question_type (question_type_id)
 );
 
 
@@ -31,3 +34,4 @@ INSERT INTO answer (answer) VALUES ('Kyllä'), ('Ei'), ('Ei koske minua');
 INSERT INTO question_type (type) VALUES ('Avoin palaute'), ('Suosittelukysymys'), ('Monivalintakysymys');
 INSERT INTO question (question, fk_questionary_id) VALUES ('Question 1', 1), ('Question 2', 1), ('Question 3',1);
 INSERT INTO questionary(name) VALUES ('Kysely 1'), ('Kysely 2'), ('Kysely 3');
+INSERT INTO question (question, fk_question_type_id, fk_questionary_id) VALUES ('Question 1', 1, 2), ('Question 2', 1, 3), ('Question 2', 1, 2), ('Question 2', 1, 1), ('Question 3', 2, 3);
